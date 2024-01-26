3dce4430bd32b47ba05ccd056f5177aacae784e3
FelipeSutter <lipeps61@gmail.com>
2024-01-26 15:56:22 -0300

    feat: created footer component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import "./vars.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Sidebar />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer class="disclaimer_premium">
+      <div class="text_disclaimer">
+        <p class="title">Testar o Premium de graça</p>
+        <p class="subtitle">
+          Inscreva-se para curtir música ilimitada e podcasts só com alguns
+          anúncios. Não precisa de cartão de crédito.
+        </p>
+      </div>
+      <button class="disclaimer_button">Inscreva-se grátis</button>
+    </footer>
+  );
+};
+
+export default Footer;
